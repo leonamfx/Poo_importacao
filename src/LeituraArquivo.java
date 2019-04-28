d59b63e2bd6a0b362b8379ebf1a9782179ddea40
Leonam Lima <leonamfx@gmail.com>
2019-04-28 19:36:16 -0300

    Alteração da Classe LeituraArquivo

--- src/LeituraArquivo.java
+++ src/LeituraArquivo.java
@@ -8,14 +8,18 @@ import java.util.List;
 
 public class LeituraArquivo {
 
+	private String arquivoName;
+	
 	public BufferedReader abrirArquivo(String arquivo) throws FileNotFoundException {
 		File file = new File(arquivo);
+		setArquivoName(file.getName());
 		FileReader fr = new FileReader(file);
 		BufferedReader br = new BufferedReader(fr);
 		return br;
 	}
 	
 	public List<String> lerArquivo(String arquivo) throws FileNotFoundException,IOException {
+		this.arquivoName = arquivo;
 		BufferedReader br = abrirArquivo(arquivo);
 		List<String> lista = new ArrayList<>();
 		while (br.ready()) {
@@ -24,18 +28,28 @@ public class LeituraArquivo {
 		return lista;
 	}
 	
-//	public static void main(String args[]) throws IOException,FileNotFoundException {
-//		String pathArquivo = System.getProperty("user.dir") + "/arquivos/"; //Define path
-//		File docs = new File(pathArquivo); //New Doc on path
-//		File []arquivos = docs.listFiles();  //Include all docs on array		
-//		
-//		for (File arquivo : arquivos) {
-//			LeituraArquivo la = new LeituraArquivo();
-//			//String dir = System.getProperty("user.dir") + "/arquivos/";
-//			List<String> lista = la.lerArquivo(pathArquivo+arquivo.getName());
-//			System.out.println(lista);
-//		}
-//	}
+	//Capiturar o nome da Classe + nome do arquivo 
+	public String getArquivoName() {
+		String[] str= arquivoName.split("_");
+        return "Processadora"+str[0].toString();
+	}
 	
+	public void setArquivoName(String arquivoName) {
+		this.arquivoName = arquivoName;
+	}
+	
+//	public static void main(String args[]) throws IOException,FileNotFoundException {
+//	String pathArquivo = System.getProperty("user.dir") + "/arquivos/"; //Define path
+//	File docs = new File(pathArquivo); //New Doc on path
+//	File []arquivos = docs.listFiles();  //Include all docs on array		
+//	
+//	for (File arquivo : arquivos) {
+//		LeituraArquivo la = new LeituraArquivo();
+//		//String dir = System.getProperty("user.dir") + "/arquivos/";
+//		List<String> lista = la.lerArquivo(pathArquivo+arquivo.getName());
+//		System.out.println(lista);
+//	}
+//}
+
 
 }
