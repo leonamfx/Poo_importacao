4a7178518b27a8147f978891f380b27baf312c35
Leonam Lima <leonamfx@gmail.com>
2019-04-28 19:44:29 -0300

    Alteração da Classe ProcessadorLinha (arg cabecalho) + Alteracao Processadora Arquivo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File arquivos[] = Getpath.getGetpath();
+		String pathAr = Getpath.getPathArquivo(); 
+	    Arrays.sort(arquivos);
+	    for (File arquivo : arquivos) {
+	            LeituraArquivo LA = new LeituraArquivo();
+	            List<String> lista = LA.lerArquivo(pathAr + arquivo.getName());
+	            String arquivoName=  LA.getArquivoName();
+	            ProcessadoraArquivo processadora = new ProcessadoraArquivo(ProcessadorInjeta.getProcessadorLinha(arquivoName)); 
+	            System.out.println(processadora.processaArquivo(lista));
+	    }
+	}
 
 	}
 
--- src/ProcessadoraArquivo.java
+++ src/ProcessadoraArquivo.java
@@ -3,27 +3,26 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-
 import model.domain.Lote;
 
 public class ProcessadoraArquivo<E extends Lote> {
 	
-	private ProcessadorLinha<E> processa;
+	private final ProcessadorLinha<E> processa;
 
 	public ProcessadoraArquivo(ProcessadorLinha<E> processa) {
 		this.processa = processa;
 	}
 
-	public List<E> processaArquivo(String arquivo)
+	public List<E> processaArquivo(List<String> listString)
 			throws IOException,ParseException,FileNotFoundException{
-		LeituraArquivo leitura = new LeituraArquivo();
+		//LeituraArquivo leitura = new LeituraArquivo();
 		List<E> listaConta = new ArrayList<>();
-		List<String> listaString = leitura.lerArquivo(arquivo);
+		//List<String> listaString = leitura.lerArquivo(arquivo);
 		//TODO implementar
-		String cabecalho = listaString.get(0);		
-		listaString.remove(0);
-		for (String linha: listaString) {
-			listaConta.add(processa.getLinha(linha));
+		String cabecalho = listString.get(0);		
+		listString.remove(0);
+		for (String linha: listString) {
+			listaConta.add(processa.getLinha(linha, cabecalho));
 		}
 		return listaConta; 		
 	}
